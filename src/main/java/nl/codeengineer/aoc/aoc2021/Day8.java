f6be00ab08deb65bf756b4bc55bfe1c66ef6bead
CodeEngineerNL <cp@codeengineer.nl>
2021-12-08 11:57:29 +0100

    Day 8

---
+++ src/main/java/nl/codeengineer/aoc/aoc2021/Day8.java
@@ -0,0 +1,146 @@
+package nl.codeengineer.aoc.aoc2021;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Day8 {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(part1());
+        System.out.println(part2());
+    }
+
+    private static long part1() throws IOException {
+        List<String> lines = getInput();
+        long easyCount = 0;
+        for (String line: lines) {
+            String[] outputs = line.split("\\|")[1].trim().split(" ");
+            HashSet<Integer> lenghts = new HashSet<>(Arrays.asList(2,4,3, 7));
+
+            for (String outputValue: outputs) {
+                if (lenghts.contains(outputValue.length())) {
+                    easyCount++;
+                }
+            }
+        }
+
+        return easyCount;
+    }
+
+    private static long part2() throws IOException {
+        List<String> lines = getInput();
+        long total = 0;
+        for (String line: lines) {
+            String[] parts = line.split("\\|");
+
+
+            List<String> uniques = Arrays.stream(parts[0].trim().split(" "))
+                    .map(Day8::sortString).collect(Collectors.toList());
+
+            List<String> output = Arrays.stream(parts[1].trim().split(" ")).map(Day8::sortString).toList();
+
+            List<String> numSegments = Arrays.asList(calcNumSegments(uniques));
+
+            int num = 0;
+            for (String value: output) {
+                num = num * 10;
+                num = num + numSegments.indexOf(value);
+            }
+
+            total = total + num;
+
+        }
+
+        return total;
+   }
+
+    private static String[] calcNumSegments(List<String> uniques) {
+        String[] numPatterns = new String[10];
+
+        for (String pattern: uniques) {
+            switch (pattern.length()) {
+                case 2 -> numPatterns[1] = pattern;
+                case 3 -> numPatterns[7] = pattern;
+                case 4 -> numPatterns[4] = pattern;
+                case 7 -> numPatterns[8] = pattern;
+                default -> { /* do nothing */}
+            }
+        }
+
+        uniques.remove(numPatterns[1]);
+        uniques.remove(numPatterns[7]);
+        uniques.remove(numPatterns[4]);
+        uniques.remove(numPatterns[8]);
+
+        // Of all numbers with 6 segments (0,6,9), only 9 contains all segments of 4
+        for (String value: uniques) {
+            if (value.length() == 6 && containsAll(value, numPatterns[4])) {
+                numPatterns[9] = value;
+            }
+        }
+
+        uniques.remove(numPatterns[9]);
+
+        // only 0 and 6 have 6 segments. 0 contains all segments of 1, 6 does not
+        for (String value: uniques) {
+            if (value.length() == 6 && containsAll(value, numPatterns[1])) {
+                numPatterns[0] = value;
+            }
+        }
+
+        uniques.remove(numPatterns[0]);
+
+        // One 6 has a length of 6 segments now.
+        for (String value: uniques) {
+            if (value.length() == 6) {
+                numPatterns[6] = value;
+            }
+        }
+
+        uniques.remove(numPatterns[6]);
+
+        // Only 2, 3 and 5 left. 3 contains all segments of 1
+        for (String value: uniques) {
+            if (containsAll(value, numPatterns[1])) {
+                numPatterns[3] = value;
+            }
+        }
+
+        uniques.remove(numPatterns[3]);
+
+        // Only 2 and 5 left. 9 contains all segemnts of 5.
+        for (String value: uniques) {
+            if (containsAll(numPatterns[9], value)) {
+                numPatterns[5] = value;
+            }
+        }
+        uniques.remove(numPatterns[5]);
+
+        // Only 2 is left now
+        numPatterns[2] = uniques.get(0);
+
+        return numPatterns;
+    }
+
+    private static boolean containsAll(String val1, String val2) {
+        for (int i = 0; i < val2.length(); i++) {
+            if (!val1.contains("" + val2.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static String sortString(String s) {
+        char[] chars = s.toCharArray();
+        Arrays.sort(chars);
+        return String.copyValueOf(chars);
+    }
+
+    private static List<String> getInput() throws IOException {
+        return Files.readAllLines(Path.of("inputs/day8-1.txt"));
+    }
+}
